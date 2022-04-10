7f0921b76288319a50be37dcec11cefce57a8a53
cfred22 <icfred@me.com>
2022-04-10 08:47:30 +0200

    init

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+// Création d'une appli express
+const app = express();
+
+// Import path qui donne accès au chemin systeme de fichier 
+const path = require('path');
+
+
+app.use((req, res) => {
+   res.json({ message: 'Votre requête a bien été reçue !' }); 
+});
+
+//Middleware pour résoudre les problemes de CORS 
+app.use((req, res, next) => {
+   res.setHeader('Access-Control-Allow-Origin', '*'); // permet d'accéder a l'api depuis n'importe quelle origine.
+   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); // permet d'ajouter des headers.
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // permet de get, post, etc..
+   next();
+});
+
+// Middleware intercepte toutes les requêtes avec content type json,
+// et met leur body directement sur l'objet req
+app.use(express.json());
+
+
+module.exports = app;
